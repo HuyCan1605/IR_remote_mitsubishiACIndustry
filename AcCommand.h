b2df3e59a7666154fd54372af9f65f5e62055960
HuyCan1605 <95010802+HuyCan1605@users.noreply.github.com>
2024-11-01 17:42:38 +0700

    comment some function

--- AcCommand.h
+++ AcCommand.h
@@ -6,14 +6,14 @@
 #define MAX_CODE_LENGTH 325
 #define MAX_CODE_BIT_LENGTH 160
 
-uint8_t encodeTemp(uint8_t temp);
-uint8_t encodeMode(uint8_t mode);
-uint8_t encodeFanMode(uint8_t fan_mode);
-uint8_t encodeSwingMode(uint8_t swing_mode);
-uint8_t encodeSwingAuto(bool swing_auto);
-uint8_t encodePower(bool swing_auto);
-uint8_t inverseBit8(uint8_t value);
-uint32_t inverseBit32(uint32_t value);
+// uint8_t encodeTemp(uint8_t temp);
+// uint8_t encodeMode(uint8_t mode);
+// uint8_t encodeFanMode(uint8_t fan_mode);
+// uint8_t encodeSwingMode(uint8_t swing_mode);
+// uint8_t encodeSwingAuto(bool swing_auto);
+// uint8_t encodePower(bool swing_auto);
+// uint8_t inverseBit8(uint8_t value);
+// uint32_t inverseBit32(uint32_t value);
 const char* returnTempForMQTT(String resultData);
 const char* returnModeForMQTT(String resultData);
 const char* returnSwingModeForMQTT(String resultData);
